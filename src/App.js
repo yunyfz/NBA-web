ffa23e57dae49f76786e1cf37d961b7d071c4713
Yilun Wang <yilunwang@YilundeMacBook-Pro.local>
2017-12-19 19:41:41 -0800

    add topnavbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { TopNavBar } from "./TopNavBar"
 
 class App extends Component {
   render() {
