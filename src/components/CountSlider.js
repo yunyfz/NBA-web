6784d5f516d7670bcf52891293e1871e211ddbb6
Yilun Wang <yilunwang@YilundeMacBook-Pro.local>
2017-12-21 21:30:39 -0800

    add search bar UI

--- src/components/CountSlider.js
+++ src/components/CountSlider.js
@@ -8,7 +8,7 @@ export class CountSlider extends React.Component {
 
     onChange = (value) => {
         this.setState({
-            inputValue: value,
+            inputValue: Number(value) || 2,
         });
         this.props.onSliderChange(value);
     }
